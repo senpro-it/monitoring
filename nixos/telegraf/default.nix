9eb814ac91c59b16817d9b4dc6bbcab869d299fb
senpro-ingwersenk <144339473+senpro-ingwersenk@users.noreply.github.com>
2024-06-17 08:48:12 +0200

    chore: Remove lib.mdDoc usage

--- nixos/telegraf/default.nix
+++ nixos/telegraf/default.nix
@@ -11,7 +11,7 @@ let
       example = literalExpression ''
         [ "udp://192.168.178.1:161" ]
       '';
-      description = lib.mdDoc ''
+      description = ''
         Endpoints which should be monitored. See the [documentation](https://github.com/influxdata/telegraf/blob/master/plugins/inputs/snmp/README.md) for syntax reference.
       '';
     };
@@ -20,7 +20,7 @@ let
         name = mkOption {
           type = types.str;
           example = "27652626525562";
-          description = lib.mdDoc ''
+          description = ''
             Context name for SNMPv3 to authenticate at the agent.
           '';
         };
@@ -30,7 +30,7 @@ let
           type = types.enum [ "MD5" "SHA" "SHA224" "SHA256" "SHA384" "SHA512" ];
           default = "MD5";
           example = "SHA";
-          description = lib.mdDoc ''
+          description = ''
             Authentication protocol used by SNMPv3 to authenticate at the agent.
           '';
         };
@@ -38,7 +38,7 @@ let
           type = types.str;
           default = "yzL9sHgeYf5NJzaeAB73014M7XrY6Aagj8UhrHbePfCfxBa99uLzVrGC8ywhfW97";
           example = "VJQUpLCLGniEDVGK8Q0oPS9Yf0xObE7m8aCDK4FR7Kzzh47MD2ZQy0dVtTkDeKBd";
-          description = lib.mdDoc ''
+          description = ''
             Password used by SNMPv3 to authenticate at the agent.
           '';
         };
@@ -48,7 +48,7 @@ let
           type = types.enum [ "DES" "AES" "AES192" "AES192C" "AES256" "AES256C" ];
           default = "MD5";
           example = "SHA";
-          description = lib.mdDoc ''
+          description = ''
             Privacy protocol used by SNMPv3 to authenticate at the agent.
           '';
         };
@@ -56,7 +56,7 @@ let
           type = types.str;
             default = "qNMR7yeaIyQ8HKfRCZU8UW5AdKM6P56UALUeYATENOn4dX3nezXELwmLgpuMWKS2";
             example = "GO61HVwspXO514vbzZiV3IwGeBnSZsjHoBaHbJU4JgEaznJ4AdVTy0wzwpzgNffz";
-            description = lib.mdDoc ''
+            description = ''
               Password used by SNMPv3 to protect to connectiont to the agent.
             '';
           };
@@ -66,7 +66,7 @@ let
           type = types.enum [ "noAuthNoPriv" "authNoPriv" "authPriv" ];
           default = "authPriv";
           example = "authPriv";
-          description = lib.mdDoc ''
+          description = ''
             Security level for SNMPv3. Look at the [documentation](https://snmp.com/snmpv3/snmpv3_intro.shtml) for further information.
           '';
         };
@@ -74,7 +74,7 @@ let
           type = types.str;
           default = "monitor";
           example = "monitor";
-          description = lib.mdDoc ''
+          description = ''
             Username for SNMPv3. Also known as `Security Name`.
           '';
         };
